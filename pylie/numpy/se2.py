FIxed SO2 jacobian and adjoint

pylie/numpy/se2.py
@@ -64,7 +64,7 @@ class SE2(MatrixLieGroup):
         phi = SO2.vee(Xi_phi)
         xi_r = Xi[0:2, 2]
         C = SO2.exp(Xi_phi)
-        r = np.dot(SO2.left_jacobian(phi), xi_r.reshape((-1, 1)))
+        r = np.dot(SE2.V_matrix(phi), xi_r.reshape((-1, 1)))
         T = np.zeros((3, 3))
         T[0:2, 0:2] = C
         T[0:2, 2] = r.ravel()
@@ -76,7 +76,7 @@ class SE2(MatrixLieGroup):
     def log(T):
         Xi_phi = SO2.log(T[0:2, 0:2])
         r = T[0:2, 2]
-        xi_r = np.dot(SO2.left_jacobian_inv(SO2.vee(Xi_phi)), r.reshape((-1, 1)))
+        xi_r = np.dot(SE2.V_matrix_inv(SO2.vee(Xi_phi)), r.reshape((-1, 1)))
         # Xi = np.block([[Xi_phi, xi_r], [np.zeros((1, 3))]])
         Xi = np.zeros((3, 3))
         Xi[0:2, 0:2] = Xi_phi
@@ -163,3 +163,26 @@ class SE2(MatrixLieGroup):
     @staticmethod
     def identity():
         return np.identity(3)
+
+    @staticmethod
+    def V_matrix(phi):
+        # Near phi==0, use first order Taylor expansion
+        if np.abs(phi) < SO2._small_angle_tol:
+            return np.identity(2) + 0.5 * SO2.wedge(phi)
+
+        s = np.sin(phi)
+        c = np.cos(phi)
+
+        return (s / phi) * np.identity(2) + ((1 - c) / phi) * SO2.wedge(1.0)
+
+    @staticmethod
+    def V_matrix_inv(phi):
+        # Near phi==0, use first order Taylor expansion
+        if np.abs(phi) < SO2._small_angle_tol:
+            return np.identity(2) - 0.5 * SO2.wedge(phi)
+
+        half_angle = 0.5 * phi
+        cot_half_angle = 1.0 / np.tan(half_angle)
+        return half_angle * cot_half_angle * np.identity(2) - half_angle * SO2.wedge(
+            1.0
+        )

pylie/numpy/so2.py
@@ -44,37 +44,29 @@ class SO2(MatrixLieGroup):
         return SO2.wedge(phi)
 
     @staticmethod
-    def left_jacobian(phi):
-        # Near phi==0, use first order Taylor expansion
-        if np.abs(phi) < SO2._small_angle_tol:
-            return np.identity(2) + 0.5 * SO2.wedge(phi)
-
-        s = np.sin(phi)
-        c = np.cos(phi)
-
-        return (s / phi) * np.identity(2) + ((1 - c) / phi) * SO2.wedge(1.0)
+    def left_jacobian(x):
+        return np.array([[1]])
+    
+    @staticmethod 
+    def left_jacobian_inv(x):
+        return np.array([[1]])
 
     @staticmethod
-    def left_jacobian_inv(phi):
-        # Near phi==0, use first order Taylor expansion
-        if np.abs(phi) < SO2._small_angle_tol:
-            return np.identity(2) - 0.5 * SO2.wedge(phi)
-
-        half_angle = 0.5 * phi
-        cot_half_angle = 1.0 / np.tan(half_angle)
-        return half_angle * cot_half_angle * np.identity(2) - half_angle * SO2.wedge(
-            1.0
-        )
+    def right_jacobian(x):
+        return np.array([[1]])
+    
+    @staticmethod
+    def right_jacobian_inv(x):
+        return np.array([[1]])
 
     @staticmethod
     def adjoint(C):
-        return np.identity(2)
+        return np.array([[1]])
 
     @staticmethod
     def odot(b):
         b = np.array(b).ravel()
         return np.array([-b[1], b[0]]).reshape((-1, 1))
-
 
     @staticmethod 
     def identity():

tests/numpy/test_so2.py
@@ -27,16 +27,14 @@ def test_group_jacobians():
 
 
 def test_adjoint_identity():
-    X = G.random()
-    xi = G.Log(G.random())
-
-    side1 = G.wedge(xi)
-    side2 = np.dot(X, np.dot(G.wedge(xi), G.inverse(X)))
-    assert np.allclose(side1, side2)
-
+    common._test_adjoint_identity(G)
 
 def test_odot_wedge():
     common._test_odot_wedge(G)
+
+
+def test_left_jacobian_numerically():
+    common._test_left_jacobian_numerically(G)
 
 
 if __name__ == "__main__":